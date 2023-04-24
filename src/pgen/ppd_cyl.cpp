b703afc12d49a8319fcf86d9056e08f4448f5f5f
jtlaune <jl3929@cornell.edu>
2023-04-24 15:19:13 -0400

    Rearrange pgen for readability

--- src/pgen/ppd_cyl.cpp
+++ src/pgen/ppd_cyl.cpp
@@ -72,32 +72,35 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
   EnrollUserHistoryOutput(5, Measurements, "momxAccretion");
   EnrollUserHistoryOutput(6, Measurements, "momyAccretion");
 
+  // AMR parameters (not used if SMR).
+  l_refine = pin->GetReal("problem", "l_refine");
+  // Hydro parameters.
   dfloor = pin->GetReal("hydro", "Sigma_floor");
-
+  // Secondary parameters.
   gm1 = pin->GetReal("problem", "GM_s");
+  soft_sat = pin->GetReal("problem", "soft_sat");
+  // Disk parameters.
   Sig0 = pin->GetReal("problem", "Sigma_0");
   dslope = pin->GetReal("problem", "delta");
   R0 = pin->GetReal("problem", "R0");
   Omega0 = pin->GetReal("problem", "Omega0");
-  soft_sat = pin->GetReal("problem", "soft_sat");
-  l_refine = pin->GetReal("problem", "l_refine");
   CS02 = SQR(pin->GetReal("hydro", "iso_sound_speed"));
-
+  // Boundary conditions.
   WDL1 = pin->GetReal("problem", "WaveDampingLength_in");
   WDL2 = pin->GetReal("problem", "WaveDampingLength_out");
   innerbdy = pin->GetReal("problem", "innerbdy");
   x1min = pin->GetReal("mesh", "x1min");
   T_damp_bdy = pin->GetReal("problem", "T_damp_bdy");
   T_damp_in = pin->GetReal("problem", "T_damp_in");
-
+  // Sink parameters.
   rSink = pin->GetReal("problem", "sink_radius");
   rEval = pin->GetReal("problem", "eval_radius");
   nPtEval = pin->GetReal("problem", "N_eval_pts");
-
   sink_dens = pin->GetReal("problem", "sink_dens");
+  // Calculation parameters.
   r_exclude = pin->GetOrAddReal("problem", "gforce_r_exclude", 0);
 
-  // enroll user-defined boundary condition
+  // Enroll user-defined boundary condition.
   if (mesh_bcs[BoundaryFace::inner_x1] == GetBoundaryFlag("user")) {
     EnrollUserBoundaryFunction(BoundaryFace::inner_x1, DiskInnerX1);
   }
@@ -105,6 +108,23 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
     EnrollUserBoundaryFunction(BoundaryFace::outer_x1, DiskOuterX1);
   }
   return;
+}
+
+Real splineKernel(Real x) {
+  // Smooth transition f(x)=0 @ x=1 and f(x)=1 @ x=0.
+  // Used for the wave damping zones at radial boundary (with appropriate
+  // definitions for x).
+  Real W;
+  if (x < 0) {
+    W = 1.;
+  } else if (x <= 0.5) {
+    W = 1. - 6. * x * x + 6. * x * x * x;
+  } else if (x <= 1) {
+    W = 2. * (1 - x) * (1 - x) * (1 - x);
+  } else {
+    W = 0.;
+  }
+  return (W);
 }
 
 Real DenProf(const Real rad) {
@@ -115,31 +135,6 @@ Real DenProf(const Real rad) {
 Real VelProf(const Real rad) {
   // Velocity profile v(r)
   return std::sqrt(dslope * CS02 + 1 / rad) - Omega0 * rad;
-}
-
-void MeshBlock::ProblemGenerator(ParameterInput *pin) {
-  // std::cout << "Problem generating is being run";
-  Real x1, x2, x3;
-  Real Sig, vk, Cx, Cy;
-  Real rprim;
-  for (int k = ks; k <= ke; ++k) {
-    x3 = pcoord->x3v(k);
-    for (int j = js; j <= je; ++j) {
-      x2 = pcoord->x2v(j);
-      for (int i = is; i <= ie; ++i) {
-        x1 = pcoord->x1v(i);
-
-        rprim = x1;
-        Sig = DenProf(rprim);
-        vk = VelProf(rprim);
-
-        phydro->u(IDN, k, j, i) = Sig;
-        phydro->u(IM1, k, j, i) = 0.;
-        phydro->u(IM2, k, j, i) = Sig * vk;
-        phydro->u(IM3, k, j, i) = 0.;
-      }
-    }
-  }
 }
 
 Real Measurements(MeshBlock *pmb, int iout) {
@@ -283,59 +278,6 @@ Real Measurements(MeshBlock *pmb, int iout) {
   }
 }
 
-Real splineKernel(Real x) {
-  // Smooth transition f(x)=0 @ x=1 and f(x)=1 @ x=0.
-  // Used for the wave damping zones at radial boundary (with appropriate
-  // definitions for x).
-  Real W;
-  if (x < 0) {
-    W = 1.;
-  } else if (x <= 0.5) {
-    W = 1. - 6. * x * x + 6. * x * x * x;
-  } else if (x <= 1) {
-    W = 2. * (1 - x) * (1 - x) * (1 - x);
-  } else {
-    W = 0.;
-  }
-  return (W);
-}
-
-void Mesh::UserWorkInLoop() {
-  Real rsecn, x1, x2, x3;
-  for (int bn = 0; bn < nblocal; ++bn) {
-    MeshBlock *pmb = my_blocks(bn);
-    // LogicalLocation &loc = pmb->loc;
-    // if (loc.level == nLevel) { // lowest level
-    for (int k = pmb->ks; k <= pmb->ke; k++) {
-      x3 = pmb->pcoord->x3v(k);
-      for (int j = pmb->js; j <= pmb->je; j++) {
-        x2 = pmb->pcoord->x2v(j);
-        for (int i = pmb->is; i <= pmb->ie; i++) {
-          x1 = pmb->pcoord->x1v(i);
-          rsecn = std::sqrt(x1 * x1 + R0 * R0 - 2 * R0 * x1 * std::cos(x2));
-          if (rsecn < rSink) {
-            pmb->phydro->w(IVX, k, j, i) = 0;
-            pmb->phydro->w(IVY, k, j, i) = 0;
-            pmb->phydro->w(IVZ, k, j, i) = 0;
-
-            // USES BAROTROPIC EOS
-            pmb->phydro->w(IDN, k, j, i) = sink_dens;
-            pmb->phydro->w(IPR, k, j, i) = sink_dens * CS02;
-
-            pmb->phydro->u(IDN, k, j, i) = sink_dens;
-            pmb->phydro->u(IM1, k, j, i) = 0;
-            pmb->phydro->u(IM2, k, j, i) = 0;
-            pmb->phydro->u(IM3, k, j, i) = 0;
-            pmb->phydro->u(IDN, k, j, i) = sink_dens;
-          }
-        }
-      }
-    }
-    //}
-  }
-  return;
-}
-
 void DiskSourceFunction(MeshBlock *pmb, const Real time, const Real dt,
                         const AthenaArray<Real> &prim,
                         const AthenaArray<Real> &prim_scalar,
@@ -406,6 +348,66 @@ void DiskSourceFunction(MeshBlock *pmb, const Real time, const Real dt,
       }
     }
   }
+}
+
+void MeshBlock::ProblemGenerator(ParameterInput *pin) {
+  Real x1, x2, x3;
+  Real Sig, vk, Cx, Cy;
+  Real rprim;
+  for (int k = ks; k <= ke; ++k) {
+    x3 = pcoord->x3v(k);
+    for (int j = js; j <= je; ++j) {
+      x2 = pcoord->x2v(j);
+      for (int i = is; i <= ie; ++i) {
+        x1 = pcoord->x1v(i);
+
+        rprim = x1;
+        Sig = DenProf(rprim);
+        vk = VelProf(rprim);
+
+        phydro->u(IDN, k, j, i) = Sig;
+        phydro->u(IM1, k, j, i) = 0.;
+        phydro->u(IM2, k, j, i) = Sig * vk;
+        phydro->u(IM3, k, j, i) = 0.;
+      }
+    }
+  }
+}
+
+void Mesh::UserWorkInLoop() {
+  Real rsecn, x1, x2, x3;
+  for (int bn = 0; bn < nblocal; ++bn) {
+    MeshBlock *pmb = my_blocks(bn);
+    // LogicalLocation &loc = pmb->loc;
+    // if (loc.level == nLevel) { // lowest level
+    for (int k = pmb->ks; k <= pmb->ke; k++) {
+      x3 = pmb->pcoord->x3v(k);
+      for (int j = pmb->js; j <= pmb->je; j++) {
+        x2 = pmb->pcoord->x2v(j);
+        for (int i = pmb->is; i <= pmb->ie; i++) {
+          x1 = pmb->pcoord->x1v(i);
+          rsecn = std::sqrt(x1 * x1 + R0 * R0 - 2 * R0 * x1 * std::cos(x2));
+          if (rsecn < rSink) {
+            pmb->phydro->w(IVX, k, j, i) = 0;
+            pmb->phydro->w(IVY, k, j, i) = 0;
+            pmb->phydro->w(IVZ, k, j, i) = 0;
+
+            // USES BAROTROPIC EOS
+            pmb->phydro->w(IDN, k, j, i) = sink_dens;
+            pmb->phydro->w(IPR, k, j, i) = sink_dens * CS02;
+
+            pmb->phydro->u(IDN, k, j, i) = sink_dens;
+            pmb->phydro->u(IM1, k, j, i) = 0;
+            pmb->phydro->u(IM2, k, j, i) = 0;
+            pmb->phydro->u(IM3, k, j, i) = 0;
+            pmb->phydro->u(IDN, k, j, i) = sink_dens;
+          }
+        }
+      }
+    }
+    //}
+  }
+  return;
 }
 
 int RefinementCondition(MeshBlock *pmb) {
